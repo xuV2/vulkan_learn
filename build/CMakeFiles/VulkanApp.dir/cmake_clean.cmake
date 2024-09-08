d267ba47727acd1b74a953e925ff058f99ea7f46
xxy <2712726205@qq.com>
2024-09-08 15:40:36 +0800

    init repo

---
+++ build/CMakeFiles/VulkanApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanApp.dir/src/test.cpp.obj"
+  "CMakeFiles/VulkanApp.dir/src/test.cpp.obj.d"
+  "VulkanApp.exe"
+  "VulkanApp.exe.manifest"
+  "VulkanApp.pdb"
+  "libVulkanApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
