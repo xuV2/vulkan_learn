2a065523a2be225da8667673991b5b64f530ab67
徐小勇 <10454760+pkgods@user.noreply.gitee.com>
2024-09-14 18:14:04 +0800

    swap_chain2

--- src/first_app.hpp
+++ src/first_app.hpp
@@ -3,12 +3,13 @@
 #include "xy_window.hpp"
 #include "xy_pipeline.hpp"
 #include "xy_device.hpp"
+#include "xy_swap_chain.hpp"
 
 
 namespace xy{
     class FirstAPP{
         public:
-            static constexpr int WIDTH = 800; // constexpr 告诉编译器这个变量的值在编译时是已知,可替换常量
+            static constexpr int WIDTH = 800; // constexpr 告诉编译器这个变量的值在编译时是已知,可替换常�?
             static constexpr int HEIGHT = 600;
             void run();
         private:
